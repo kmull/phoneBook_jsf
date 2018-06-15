2721646e64464e82b4eece5795e7394171a38854
Krzysztof Muller <krzysztofmuller1@gmail.com>
2018-06-15 15:39:55 +0200

    navigation layout

--- src/main/java/com/muller/myTelephoneBook/Navigation/NavigationBean.java
+++ src/main/java/com/muller/myTelephoneBook/Navigation/NavigationBean.java
@@ -12,62 +12,67 @@ import java.io.Serializable;
 @RequestScoped
 public class NavigationBean implements Serializable {
 
+    private final String errorNotFound = "Not Found";
+
     Logger log = LoggerFactory.getLogger(NavigationBean.class);
     @ManagedProperty(value = "#{param.pageId}")
     private String pageId;
 
     public String showPage() {
         log.info("show page navig, option: " + pageId);
+
         if (pageId == null) {
+            log.info(errorNotFound);
             return "index";
         }
+
         if (pageId.equals("1")) {
             return "addUser";
         } else if (pageId.equals("2")) {
-            return moveTo_getUsers();
+            return "getUsers";
         } else if (pageId.equals("3")) {
-            return moveTo_myTest();
+            return "myTest";
         } else if (pageId.equals("4")) {
-            return moveTo_findByName();
-        } else {
+            return "findByName";
+        } else if (pageId.equals("0")) {
+            return "index";
+        }else {
             return "index";
         }
-    }
-
-    public String moveTo_addUser() {
-        return "addUser";
-    }
-
-    public String moveTo_getUsers() {
-        return "getUsers";
-    }
-
-    public String moveTo_myTest() {
-        return "myTest";
-    }
-
-    public String moveTo_findByName() {
-        return "findByName";
-    }
-
-    public String moveTo_index() {
-        return "index";
-    }
-
-    public String moveTo_myNavig() {
-        return "myNavig";
-    }
-
-    public String moveTo_findResult() {
-        return "findByNameResult";
     }
 
     public String getPageId() {
         return pageId;
     }
-
     public void setPageId(String pageId) {
         this.pageId = pageId;
     }
-
 }
+
+//    public String moveTo_addUser() {
+//        return "addUser";
+//    }
+//
+//    public String moveTo_getUsers() {
+//        return "getUsers";
+//    }
+//
+//    public String moveTo_myTest() {
+//        return "myTest";
+//    }
+//
+//    public String moveTo_findByName() {
+//        return "findByName";
+//    }
+//
+//    public String moveTo_index() {
+//        return "index";
+//    }
+//
+//    public String moveTo_myNavig() {
+//        return "myNavig";
+//    }
+//
+//    public String moveTo_findResult() {
+//        return "findByNameResult";
+//    }
